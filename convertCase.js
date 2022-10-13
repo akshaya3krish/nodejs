fa3b439014a4a9341578d1a5989ac829c654ecd3
akshaya3krish <akshaya3krish@gmail.com>
2022-10-13 16:17:41 +0530

    convertCase program

---
+++ convertCase.js
@@ -0,0 +1,5 @@
+//program to convert all the words to upper and lower case
+const text = "Hello welcome to Nodejs Batch-3"; //Given the message to convert 
+console.log(text.toUpperCase()); // printing the message to Uppercase
+console.log(text.toLowerCase()); // printing the message to Lowercase
+//end of the program
