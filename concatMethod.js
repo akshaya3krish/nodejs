08bd193bab296cf92b9f0410a9ed68e7af02536f
akshaya3krish <akshaya3krish@gmail.com>
2022-10-13 16:30:58 +0530

    Concatenate Method

---
+++ concatMethod.js
@@ -0,0 +1,6 @@
+//program to concatenate two strings
+const str1 = "Nodejs"; //given value is string1
+const str2 = "Batch3"; //given value in string2
+const newString = str1.concat(" ",str2);//giving concat() method to combine two strings and assigning value to the result string
+console.log(newString); //printing the concatenated string result
+//end of the program
