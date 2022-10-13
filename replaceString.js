43bef78e625da8ac6c1699b94ea52adbf23dac04
akshaya3krish <akshaya3krish@gmail.com>
2022-10-13 16:02:38 +0530

    replaceStringProgram

---
+++ replaceString.js
@@ -0,0 +1,6 @@
+//program to replace string
+const str =  new String("I am from Hyderabad"); // Initialising string
+console.log("Original string :" , str); //printing the string
+const replacedString = str.replace("Hyderabad", "Bangalore"); // replacing hyderabad with bangalore using str.replace
+console.log("After replacing Hyderabad with Bangalore: ", replacedString); //printing the replaced string message
+//end of the replace string program.
