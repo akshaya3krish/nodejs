6b00b3d1763da33efb89aeabbc3927ed39ce0504
akshaya3krish <akshaya3krish@gmail.com>
2022-10-14 16:47:23 +0530

    join method

---
+++ joinMethod.js
@@ -0,0 +1,5 @@
+//program to join strings
+const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]; // assigning array values to the variable
+const str = arr.join(",,"); // The join() method of the Array is used to join the elements of an array into a string, 
+console.log(str); // printing the output after joining the given value
+//end of the program
