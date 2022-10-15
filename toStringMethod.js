fbaaa164f88e3aabbe3c1d0f61bef15e79766c77
akshaya3krish <akshaya3krish@gmail.com>
2022-10-15 15:26:48 +0530

    toString() method

---
+++ toStringMethod.js
@@ -0,0 +1,7 @@
+//program on toString() method
+const arg = [2,3,4,5,6,7,8,'abcd']; // assigning values to the variable
+const arg1 = (arg.toString()); //converting the values to string using toString() method
+const arg2 = (`${arg}`); // converting the values to string using `${}` method
+console.log(arg1); //printing the result with totring() method
+console.log(arg2); //printing the result with `${}`method
+//end of the program
