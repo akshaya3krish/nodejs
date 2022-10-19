e13a25bf8af8004cb23cc23232557e74899f7667
akshaya3krish <akshaya3krish@gmail.com>
2022-10-19 09:28:12 +0530

    unshift Method

---
+++ unshiftMethod.js
@@ -0,0 +1,13 @@
+//program to unshift the element using unshift() method
+const classNames = ["Arun", "Faiz", "Roshan","Sakshi", "Yashwanth"]; //giving values to the variable
+const className = classNames.unshift("Akshaya"); //The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
+console.log(className); //The unshift() method returns the new array length.
+console.log(classNames); //printing the values with new value value
+//end of the program
+
+//program to unshift the element using unshift() method
+const a = [10,20,30,40]; //giving values to the variable
+const b = a.unshift(5); //The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
+console.log(b); //The unshift() method returns the new array length.
+console.log(a); //printing the values with new value value
+//end of the program
