4bfee81347e1c41e743536eb28537d8213685c9f
akshaya3krish <akshaya3krish@gmail.com>
2022-10-28 16:58:55 +0530

    example program on callback

---
+++ callback1.js
@@ -0,0 +1,76 @@
+function sub(a, b, callback){
+    setTimeout(() => {
+        const resSub = a - b;
+        callback(resSub);
+    }, 1000)
+}
+
+function sub1 (num, type, callback){
+    if (type === "Even") {
+        num = num * 10;
+        callback(num)
+    }
+    else{
+        num = num * 5;
+        callback(num);
+    }
+}
+sub (100, 19, (result) => {
+    console.log("Result is:", result);
+    if(result % 2 === 0){
+        console.log("Even Number");
+        sub1(result, "Even", (resultEvenOrOdd) => {
+            console.log(resultEvenOrOdd);
+                if(result<0){
+                    console.log("Number is below 0");
+                    if(result<=0){
+                        const newResult = result * 100 + "\n new result multiply by 100";
+                        console.log(newResult);
+                    }
+                    else{
+                        const newResult = result - 100 + "\n new result subtract with 100";
+                        console.log(newResult);
+                    }
+                }
+                else{
+                    console.log("number is above 0");
+                    if(result>=0){
+                        const newResult = result * 100 + "\n new result multiply by 100";
+                        console.log(newResult);
+                    }
+                    else{
+                        const newResult = result - 100 + "new result subtract with 100";
+                        console.log(newResult);
+                    }
+                }
+        })
+    }
+    else{
+        console.log("Odd Number");
+        sub1(result, "Odd", (resultEvenOrOdd) => {
+            console.log(resultEvenOrOdd);
+            if(result<0){
+                console.log("Number is below 0");
+                if(result<=0){
+                    const newResult = result * 100 + "\n new result multiply by 100";
+                    console.log(newResult);
+                }
+                else{
+                    const newResult = result - 100 + "\n new result subtract with 100";
+                    console.log(newResult);
+                }
+            }
+            else{
+                console.log("number is above 0");
+                if(result<=0){
+                    const newResult = result * 100 + "\n new result multiply by 100";
+                    console.log(newResult);
+                }
+                else{
+                    const newResult = result - 100 + "\n new result subtract with 100";
+                    console.log(newResult);
+                }
+            }
+        })
+    }
+});
