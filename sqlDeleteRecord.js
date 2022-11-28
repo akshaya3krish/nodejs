afdb72c13c66339d71129041dd344cd310ff6be9
akshaya3krish <akshaya3krish@gmail.com>
2022-11-28 19:56:47 +0530

    SQL Delete Record Program

---
+++ sqlDeleteRecord.js
@@ -0,0 +1,32 @@
+const sql = require('mssql');
+
+const sqlConfigLocal = {
+    user: 'akshaya003',
+    password: 'welcome123',
+    database: 'nodejs',
+    server: 'localhost',
+    options: {
+        trustServerCertificate: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+};
+
+const conn = async () => {
+    try {
+        await sql.connect(sqlConfigLocal);
+       const result = await sql.query`DELETE FROM Employee WHERE City = 'MP'`;
+       return Promise.resolve(result);
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+conn().then(result => {
+    console.log(`Record Deleted successfully: ${JSON.stringify(result)}`);
+}).catch(err => {
+    console.error(` In error : ${err}`);
+});
